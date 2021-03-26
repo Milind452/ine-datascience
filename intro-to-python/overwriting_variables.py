3179a171e1fae1f1fe1dabd1994607962bd6f294
Milind452 <mohapatra.milind@gmail.com>
2021-03-26 18:03:46 +0530

    Overwriting variables assignment

---
+++ intro-to-python/overwriting_variables.py
@@ -0,0 +1,13 @@
+def test_overwrite():
+    assert old_variable == "hats"
+    assert another_old_variable != 42
+
+if __name__ == "__main__":
+    old_variable = "pants"
+    another_old_variable = 42
+
+    # Overwrite "old_variable" and change it's value from "pants" to "hats"
+    old_variable = "hats"
+
+    # Overwrite "another_old_variable" to something different on the line below
+    another_old_variable = 100
