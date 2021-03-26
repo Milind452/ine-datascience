65de6927d914b4b458a66a18ff4bf40041eef9e2
Milind452 <mohapatra.milind@gmail.com>
2021-03-26 18:13:54 +0530

    Add using shorthand assignment

---
+++ intro-to-python/shorthand.py
@@ -0,0 +1,17 @@
+def test_shorthand():
+    assert change_me == 61, "Wrong value"
+
+if __name__ == "__main__":
+    change_me = 34
+
+    # Use the shorthand to multiply change_me by 7 and store the result in change_me
+    change_me *= 7
+
+    # Now add 23 to change_me
+    change_me += 23
+
+    # Subtract 17 from change_me
+    change_me -= 17
+
+    # Divide change_me by 4
+    change_me /= 4
