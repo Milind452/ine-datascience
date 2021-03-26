0aedd4f19edd8142d7991d6023e101f8331528d4
Milind452 <mohapatra.milind@gmail.com>
2021-03-26 18:16:10 +0530

    Add in or out assignment

---
+++ intro-to-python/in_or_out.py
@@ -0,0 +1,9 @@
+def test_in_or_out():
+    assert pro_in_programming is True
+    assert w_in_bridge is False
+
+if __name__ == "__main__":
+    # Complete the following operations
+    pro_in_programming = 'c' in 'cat'
+
+    w_in_bridge = 1 in [2, 3, 4]
