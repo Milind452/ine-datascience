d120a07bf76acc4b584468d171082e411422f5eb
Milind452 <mohapatra.milind@gmail.com>
2021-03-26 18:06:20 +0530

    Add practicing with strings assignment

---
+++ intro-to-python/practicing_with_strings.py
@@ -0,0 +1,10 @@
+def test_final_text():
+    assert text == "Learning Python with INE"
+
+if __name__ == "__main__":
+    string_1 = "Learning Python"
+    string_2 = "with INE"
+
+    # Final text should be:
+    # "Learning Python with INE"
+    text = string_1 + " " + string_2
