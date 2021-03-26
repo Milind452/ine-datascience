777b25a313447df2cd7e3806d656422ed2d47880
Milind452 <mohapatra.milind@gmail.com>
2021-03-26 18:25:17 +0530

    Add print list values assignment

---
+++ intro-to-python/print_list.py
@@ -0,0 +1,15 @@
+def test_while():
+    assert type(my_list) == list, "Should be a List"
+    assert type(i) == int, "Should be an Integer"
+    assert i == len(my_list), "Should print all list values"
+
+if __name__ == "__main__":
+    my_list = [True, 'Programming', 'Python', 'with', 'INE', 123]
+    i = None
+
+
+    i = len(my_list)
+    x = 0
+    while x < i:
+        print(my_list[x])
+        x += 1
